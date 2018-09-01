6a7dc829cc126e15c866f336ebf6755f3f6cd8f7
Alex23rodriguez <alex.rodriguez.oro@gmail.com>
2018-09-01 07:54:35 -0500

    some buttons, roaming

--- E8/sketch.js
+++ E8/sketch.js
@@ -22,8 +22,9 @@ wiggle = []
 colors = []
 nums = []
 
-ups = []
-downs = []
+var btn, ups, downs;
+
+roaming = []
 
 //cons = 0
 //rotation = [.01,0.02,.03,.04,-.05]
@@ -36,7 +37,6 @@ inward = false
 //layerModes= [l,floor((int(l)+int(l2))/2), round((int(l)+int(l2))/2), ceil((int(l)+int(l2))/2), l2]
 
 //radiusMode = 1
-
 
 
 var points = []
@@ -76,7 +76,24 @@ function budge(points){
     }
     //print(newP)
     return newP
-    
+}
+
+function roam(){
+    for (r of roaming){
+        //print(r[0].value())
+        r[0].value( r[0].value() + random(-r[1], r[1]) );
+        r[0].value( max(r[0].value(), r[2]) )
+        r[0].value( min(r[0].value(), r[3]) )
+        //print(r[0].value())
+    }
+}
+
+function addRoam(a, b, c, d){
+    if(c==undefined){
+        c = int(a.elt.min);
+        d = int(a.elt.max);
+    }
+    roaming.push([a, b, c, d]);
 }
 
 function drawPoints(){
@@ -134,13 +151,13 @@ function setup() {
     
     for(i = 0; i < layers; i++){
         
-        downs.push(createButton('-'))
-        downs[i].position(40,30*i+50)
-        downs[i].mousePressed(fgen(i,false))
+        downs = createButton('-')
+        downs.position(40,30*i+50)
+        downs.mousePressed(fgen(i,false))
         
-        ups.push(createButton('+'))
-        ups[i].position(60,30*i+50)
-        ups[i].mousePressed(fgen(i, true))
+        ups = createButton('+')
+        ups.position(60,30*i+50)
+        ups.mousePressed(fgen(i, true))
         
         rots.push(createSlider(-.05, .05, 0, .0005));
         rots[i].position(130,30*i+50)
@@ -155,23 +172,21 @@ function setup() {
         bright.push(createSlider(0,100,50+(50/layers*i),.1))
         bright[i].position(300, 30*i+250)
     }
+    ups = []
   
     wiggle.push(createSlider(0,4,0,.1))
     wiggle.push(createSlider(0,4,0,0.01))
-    wiggle[0].position(300, 460)
-    wiggle[1].position(300, 490)
+    wiggle[0].position(300, 490)
+    wiggle[1].position(300, 520)
       
     radii.push(createSlider(-.005,.005,0,.00005)) // oscilation in radius x
     radii.push(createSlider(-.05,.05,0,.0005)) // offset per layer x
     radii.push(createSlider(-.005,.005,0,.00005)) // oscilation in radius y
     radii.push(createSlider(-.05,.05,0,.0005)) // offset per layer y
-    radii[0].position(20, 460)
-    radii[1].position(20, 490)
-    radii[2].position(160, 460)
-    radii[3].position(160, 490)
-    
-    strokeW = createSlider(0.05,2,0.7, 0.05)//layerModes.lenght-1,0,1)
-    strokeW.position(300, 50)
+    radii[0].position(20, 490)
+    radii[1].position(20, 520)
+    radii[2].position(160, 490)
+    radii[3].position(160, 520)
     
     radiusSpread = createSlider(0,3,1,0.02)//layerModes.lenght-1,0,1)
     radiusSpread.position(300, 100)
@@ -179,8 +194,77 @@ function setup() {
     layerMode = createSlider(0,3,0,1)//layerModes.lenght-1,0,1)
     layerMode.position(300, 150)
     
-    fade = createSlider(0,0.5, 0.5, 0.0005)//layerModes.lenght-1,0,1)
-    fade.position(300, 200)
+    strokeW = createSlider(0.05,2,0.7, 0.05)//layerModes.lenght-1,0,1)
+    strokeW.position(300, 50)
+    
+    fade = createSlider(0,0.5, 0.5, 0.0005);//layerModes.lenght-1,0,1)
+    fade.position(300, 200);
+      
+    //reset oscilation
+    btn = createButton('Freeze');
+    btn.position(40,550);
+    btn.mousePressed(
+        function(){
+            radii[0].value(0);
+            radii[1].value(0);
+        }
+    );
+    
+    btn = createButton('Reset');
+    btn.position(90,550);
+    btn.mousePressed(
+        function(){
+            rad[0] = 0;
+            rad[1] = 0;
+        }
+    );
+    
+    btn = createButton('Freeze');
+    btn.position(175,550);
+    btn.mousePressed(
+        function(){
+            radii[2].value(0);
+            radii[3].value(0);
+        }
+    );
+    
+    btn = createButton('Reset');
+    btn.position(225,550);
+    btn.mousePressed(
+        function(){
+            rad[2] = 0;
+            rad[3] = 0;
+        }
+    );
+    
+    // randomize color
+    btn = createButton('Randomize');
+    btn.position(50, 423);
+    btn.mousePressed(
+        function(){
+            for(s of hues){
+                s.value(random(360));
+            }
+        }
+    );
+    btn = createButton('Randomize');
+    btn.position(190, 423);
+    btn.mousePressed(
+        function(){
+            for(s of sat){
+                s.value(random(100));
+            }
+        }
+    );
+    btn = createButton('Randomize');
+    btn.position(330, 423);
+    btn.mousePressed(
+        function(){
+            for(s of bright){
+                s.value(random(100));
+            }
+        }
+    );
       
     
       //print(rotation)
@@ -223,7 +307,8 @@ function draw() {
     //print(budge(points))
     points = budge(points)
     drawPoints()
-    //print(rot)
+    
+    roam()
     //rad+=.01
     //print(cos(r))
     for(i = 0; i < layers; i++){
@@ -237,7 +322,7 @@ function draw() {
     pop()
     
     fill(20)
-    rect(10,20,440,510)
+    rect(10,20,440,560)
     fill(255)
     
     textSize(12)
@@ -246,22 +331,19 @@ function draw() {
     text('Width', 350, 40)
     text('Spread', 350, 90)    
     text('Color Mode', 335, 140) 
-    text('Fade', 335, 190) 
+    text('Fade', 350, 190) 
     text('Color per layer (Hue / Saturation / Brightness)', 100, 242) 
-    text('X Oscilation', 55, 450)
-    text('Y Oscilation', 195, 450)
-    text('Jiggle', 350, 450)
+    text('X Oscilation', 55, 480)
+    text('Y Oscilation', 195, 480)
+    text('Jiggle', 350, 40)
     stroke(255)
     line(10,223,450,223)
-    line(10,420,450,420)
+    line(10,450,450,450)
     
     for(i = 0; i < layers; i++){
         text(points[i].length,20,30*i+60)
     }
-    
-  //print(rots[0].value())
-  //print("hell")
-  
+
 }
 
 
